fdf99092b6f227beca051491d1ccd9ebd5b385ba
Marcin Miazga <miazgamarcin@outlook.com>
2018-09-13 19:35:10 +0200

    update docker nuget push script

--- scripts/docker-nuget-publish.sh
+++ scripts/docker-nuget-publish.sh
@@ -11,4 +11,3 @@ export NUGET_API_KEY
 DOCKER_COMPOSE_PATH=`readlink -f "../Outloud/docker-compose.${ENVIRONMENT}.yml"`
 
 docker-compose -f $DOCKER_COMPOSE_PATH up --abort-on-container-exit $IMAGE_NAME
-read -p "Press enter to continue"
